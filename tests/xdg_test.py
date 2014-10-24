0b3eda4351d4795671715fc934ee26cfcdae06ab
Nathan Typanski <ntypanski@gmail.com>
2014-10-23 20:59:21 -0400

    test cases for xdg configuration

--- tests/xdg_test.py
+++ tests/xdg_test.py
@@ -16,3 +16,11 @@ class TestCase(unittest.TestCase):
     def test_get_xdg_config_home(self):
         xdg = fool.xdg.XDG()
         self.assertIsInstance(xdg.config_home, str)
+
+    def test_config_data_home_ends_with_fool(self):
+        conf = fool.xdg.Configuration()
+        self.assertTrue(conf.data_dir.endswith('fool'))
+
+    def test_config_config_home_ends_with_fool(self):
+        conf = fool.xdg.Configuration()
+        self.assertTrue(conf.config_dir.endswith('fool'))

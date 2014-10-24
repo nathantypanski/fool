3b3877bacafbddc5b02f4c9ba230d6a9cbf7de04
Nathan Typanski <ntypanski@gmail.com>
2014-10-23 20:43:18 -0400

    add more initial test garbage to main script

--- fool.py
+++ fool.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 
-""" fool - a dotfile sync toolkit """
+"""fool - a dotfile sync toolkit"""
+from __future__ import print_function
 
 import argparse
-import sys
 import fool.git
+import fool.xdg
 
 def parse_args():
     args = argparse.ArgumentParser()
@@ -14,6 +15,8 @@ def parse_args():
 def main():
     args = parse_args()
     print(fool.git.which_git())
+    xdg = fool.xdg.XDG()
+    print(xdg.data_home)
 
 if __name__ == '__main__':
     main()
